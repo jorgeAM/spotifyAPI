209c60332acdb806293f830465bc096e4b16045e
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-21 23:13:42 -0500

    Modelo Artist

---
+++ models/artist.js
@@ -0,0 +1,13 @@
+//libreria para usar la conexion a db
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//creamos esquema
+var artistSchema = new Schema({
+	name: String,
+	description: String,	
+	image: String
+});
+
+//exportamos el modelo
+module export = mongoose.model('Artist', artistSchema);
