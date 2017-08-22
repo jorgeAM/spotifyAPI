3443d2364e8e9d45da364f8da8a7ef582669fcac
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-21 23:20:43 -0500

    Modelo Album

---
+++ models/album.js
@@ -0,0 +1,18 @@
+//libreria para usar la conexion a db
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//creamos esquema
+var albumSchema = new Schema({
+	title: String,
+	description: String,
+	year: String,
+	image: String,
+	artist: {
+		type: Schema.Types.ObjectId,
+		ref: 'Artist'
+	}
+});
+
+//exportamos el modelo
+module export = mongoose.model('Album', albumSchema)
