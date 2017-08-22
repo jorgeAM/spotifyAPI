a19bbb10605fb2aec6f2acd7c2b128e9a6f4e740
Jorge L. Alfaro <jorge.alfmur@gmail.com>
2017-08-21 23:27:12 -0500

    Modelo Song

---
+++ models/song.js
@@ -0,0 +1,18 @@
+//libreria para usar la conexion a db
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//creamos esquema
+var songSchema = new Schema({
+	number: String,
+	name: String,
+	duration: String,
+	file: String,
+	album: {
+		type: Schema.Types.ObjectId,
+		ref: 'Album'
+	}
+});
+
+//exportamos el modelo
+module export = mongoose.model('Song', songSchema)
